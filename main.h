f498459f21549a544202cd6661eb540629a3a784
Temitope0001 <adesinatemitope0001@gmail.com>
2022-10-18 11:58:55 -0700

    added string_helper.c file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct format - match each conversion specifier for printf
+ * @id: type char pointer of the specifier i.e
